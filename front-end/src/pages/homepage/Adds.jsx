3e1a1611bdff76ad3e927fb47e37146f0212d978
phani <phanidharguttikonda0@gmail.com>
2024-11-05 22:14:37 +0530

    added responsiveness for adds.js for devices between 280 to 340px

--- front-end/src/pages/homepage/Adds.jsx
+++ front-end/src/pages/homepage/Adds.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import imdb from '../../Images/imdb.png';
 import css from './Adds.module.css';
-
+ 
 function Adds(props) {
     console.log(props.data)
     return (
@@ -11,7 +11,7 @@ function Adds(props) {
                 <img src={props.data.title} alt="" className={css.titleposter}/>
                 <div className={css.details}>
                     <span className={css.s} style={{"color": 'White'}}> <img src={imdb} alt="" /> {props.data.imdb}/10</span>
-                    <h6> <span className={css.sd}>{props.data.views}</span> Streams</h6>
+                    <h6 className={css.str}> <span className={css.sd}>{props.data.views}</span> Streams</h6>
                 </div>
                 <div className={css.buttons}>
                     <button className={css.p}>Play</button>
